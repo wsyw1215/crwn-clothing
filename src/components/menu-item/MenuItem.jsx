ba93b9a85c818053e73ac3ac346c8e53bace5e9d
Eileen <eileen19930207@gmail.com>
2019-09-12 17:15:41 +0800

    add homepage

---
+++ src/components/menu-item/MenuItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./MenuItem.scss";
+import { withRouter } from "react-router-dom";
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  return (
+    <div className={`menu-item ${size}`} onClick={()=>{ history.push(`${match.url}${linkUrl}`)}}>
+      <div className="background-image" style={{ backgroundImage: `url(" ${imageUrl} ")` }} />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
