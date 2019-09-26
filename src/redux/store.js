c39c40f23234ecb53d374da5d33000ee6bd2c8f5
eileen <eileen@mail.js-tech.tw>
2019-09-26 09:42:34 +0800

    add middleware logger  in development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+// 只有開發模式才讓redux狀態print出來(middleware加入logger)
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 export default { store, persistor };
